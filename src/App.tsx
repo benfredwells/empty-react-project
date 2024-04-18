ce4a573cd47e8e9d02490f543e9dd718b50fabd7
benfred <benfred@mjaw.com>
2024-04-18 15:48:04 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex flex-col">
+      <div>
+        Hello world!
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
